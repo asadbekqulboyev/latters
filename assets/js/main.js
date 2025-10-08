0b41bd087008a7acfd610d820795bfd221302726
asadbek <asadbekqulboyev@gmail.com>
2025-10-08 15:48:58 +0500

    finish 70%

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // phone inputmask
+  //   const phoneInput = document.querySelector("input[name=phone]");
+  //   Inputmask({
+  //     mask: "+7 (999) 999-99-99",
+  //     clearIncomplete: true,
+  //     showMaskOnHover: false,
+  //     showMaskOnFocus: true,
+  //     placeholder: "_",
+  //   }).mask(phoneInput);
+  //
+  const faqItems = document.querySelectorAll('.faq_item');
+
+  faqItems.forEach(item => {
+    const header = item.querySelector('.faq_header');
+    const content = item.querySelector('.faq_content');
+  
+    header.addEventListener('click', () => {
+      const isOpen = item.classList.contains('active');
+  
+      // Boshqalarni yopish (agar kerak bo‘lsa)
+      faqItems.forEach(i => {
+        i.classList.remove('active');
+        const c = i.querySelector('.faq_content');
+        c.style.height = 0;
+        c.style.opacity = 0;
+      });
+  
+      // Agar avval yopiq bo‘lsa — ochamiz
+      if (!isOpen) {
+        item.classList.add('active');
+        content.style.height = content.scrollHeight + 'px';
+        content.style.opacity = 1;
+      }
+    });
+  });
+  
+});
